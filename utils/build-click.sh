2c6b921beb9a0b59fe2bfdc4bcc64e59192f732d
LNJ <git@lnj.li>
2018-03-31 15:34:41 +0200

    dfg

--- utils/build-click.sh
+++ utils/build-click.sh
@@ -47,8 +47,8 @@ install_dependencies() {
 }
 
 build_kaidan() {
-	ls ${CLICK_TARGET_DIR}/usr/lib/pkgconfig
-	export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:${CLICK_TARGET_DIR}/usr/lib/pkgconfig"
+	ls ${CLICK_TARGET_DIR}/lib/pkgconfig
+	export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:${CLICK_TARGET_DIR}/lib/pkgconfig"
 
 	mkdir -p $KAIDAN_SOURCES/build
 	cd $KAIDAN_SOURCES/build
